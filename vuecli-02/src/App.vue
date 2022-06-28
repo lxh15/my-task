cfc5966b4738674fd82c28ebfadd2f1580a1ffea
lxh15 <1392649188@qq.com>
2022-06-28 20:24:40 +0800

    on事件修饰符

--- vuecli-02/src/App.vue
+++ vuecli-02/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
-    <!-- v-bind的练习 -->
-    <a v-bind:href="url">点击去百度</a>
-    <br />
-    <a :href="url">点击去百度简写</a>
+  <div @click="father">
+    <!-- v-on -->
+    <a :href="url" @click.prevent>点击组织默认事件</a>
+    <p @click.stop="pFn">阻止冒泡</p>
+    <button @click.once.stop="btn">只执行一次</button>
   </div>
 </template>
 
@@ -13,6 +13,17 @@ export default {
     return {
       url: 'http://www.baidu.com',
     };
+  },
+  methods: {
+    father() {
+      console.log('father');
+    },
+    pFn() {
+      console.log('p');
+    },
+    btn() {
+      console.log('button');
+    },
   },
 };
 </script>
---
+++ vuecli-02/src/bind指令.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <!-- v-bind的练习 -->
+    <a v-bind:href="url">点击去百度</a>
+    <br />
+    <a :href="url">点击去百度简写</a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      url: 'http://www.baidu.com',
+    };
+  },
+};
+</script>
+
+<style></style>
