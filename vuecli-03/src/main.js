86f9083cbc3dc325641538c0e18843205ec9e056
lxh15 <1392649188@qq.com>
2022-06-30 21:06:46 +0800

    品牌管理完成

--- vuecli-03/src/main.js
+++ vuecli-03/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
+import "bootstrap/dist/css/bootstrap.css" // 默认找文件夹下的index文件(但是这个不是所以需要写路径)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
